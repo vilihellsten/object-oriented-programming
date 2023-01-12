d12b5f13c22eaf887c7355afea44bad57d1bbf35
vilihellsten <vilihellsten@hotmail.com>
2023-01-12 09:59:23 +0200

    12.1. car excersice

---
+++ CarExcercise/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarExcercise
+{
+    internal class Car
+    {
+        string merkki;
+        double nopeus;
+
+        public Car()
+        {
+            
+        }
+
+        public void AskData()
+        {
+            Console.WriteLine("Anna auton merkki");
+            this.merkki = Console.ReadLine();
+            Console.WriteLine("Anna auton nopeus");
+            this.nopeus = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine(this.merkki +" " + this.nopeus);
+        }
+
+        public void Accelerate(int nopeus)
+        {
+            if (nopeus > this.nopeus)
+                this.nopeus = nopeus;
+            else
+                this.nopeus = this.nopeus;
+
+        }
+
+        public void Brake()
+        {
+            this.nopeus = this.nopeus * 0.9;
+        }
+    }
+}
---
+++ CarExcercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using CarExcercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        firstCar.AskData();
+        secondCar.AskData();
+        firstCar.ShowCarInfo();
+        secondCar.ShowCarInfo();
+        firstCar.Accelerate(210);
+        firstCar.ShowCarInfo();
+        secondCar.Brake();
+        secondCar.ShowCarInfo();
+    }
+}
