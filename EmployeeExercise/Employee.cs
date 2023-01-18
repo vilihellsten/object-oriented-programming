ceed665b63b484381d831c4ff5ada38fac47450f
vilihellsten <vilihellsten@hotmail.com>
2023-01-18 14:20:54 +0200

    18.1

---
+++ EmployeeExercise/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    internal class Employee
+    {
+        string name;
+        string id;
+        string position;
+        double salary;
+
+        public Employee()
+        {
+            this.name = string.Empty;
+            this.id = string.Empty;
+            this.position = string.Empty;
+            this.salary = 0;
+        }
+
+        public void CompareSalary(Employee employees)
+        {
+            if (this.salary > employees.salary)
+                Console.WriteLine(this.name + ", " + this.salary + ", " + employees.name + ", " + employees.salary + ", Henkilön " + this.name + " palkka on suurempi.");
+            else
+                Console.WriteLine(this.name + ", " + this.salary + ", " + employees.name + ", " + employees.salary + ", Henkilön " + employees.name + " palkka on suurempi.");
+        }
+
+        public Employee(string v1, string v2, string v3, int v4)
+        {
+            this.name = v1;
+            this.id = v2;
+            this.position = v3;
+            this.salary = v4;
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine(this.name +", " + this.id + ", " + this.position + ", " +this.salary);
+        }
+
+        public override string? ToString()
+        {
+            return this.name + ", " + this.id + ", " + this.position +", " + this.salary;
+        }
+    }
+}
---
+++ EmployeeExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+using EmployeeExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee("Pekka","1","Johtaja",3000);
+        employees[1] = new Employee("Mari", "2", "Sihteeri", 5000);
+        employees[2] = new Employee("Keijo", "3", "Siivooja", 1500);
+
+        Console.WriteLine("\nVertaa parametrina saamaansa työntekijän palkkaa kutsuttavan olion palkkaan.");
+        employees[0].CompareSalary(employees[1]);
+
+        Console.WriteLine("\nTulostaa työntekijän tiedot PrintEmployeeInfon avulla.");
+        employees[0].PrintEmployeeInfo();
+
+        Console.WriteLine("\nTulostaa työntekijän tiedot ToStringin avulla.");
+        Console.WriteLine(employees[0].ToString());
+
+
+        Console.WriteLine("\nTulostaa vertailtavat henkilöt ja heidän palkkansa sekä vertailee palkan suuruutta keskenään for loopilla.");
+        int n = 1;        
+        for(int i = 0; i < employees.Length; i++)
+        {
+            if (n == employees.Length)
+            {
+                n = 0;
+            }
+                employees[i].CompareSalary(employees[n]);
+                n++;
+        }
+        
+    }
+
+}
