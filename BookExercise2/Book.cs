8b8b2965f274c23705cccdd72667461f89c288b5
vilihellsten <vilihellsten@hotmail.com>
2023-01-25 11:41:06 +0200

    25.1

---
+++ BookExercise2/Book.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExercise2
+{
+    internal class Book
+    {
+        private string name;
+        private string author;
+        private string publisher;
+        private double price;
+        private string isbn;
+        private static string theme;
+
+        private const int MAX_LENGTH = 13;
+        private const string PREFIX = "978";
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                if (value > 30)
+                {
+                    price = value * 0.9;
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public string Isbn
+        {
+            get
+            {
+                return isbn;
+            }
+
+            set
+            {
+                bool cont = true;
+
+                while (cont)
+                if (value.Length == MAX_LENGTH && value.StartsWith(PREFIX))
+                {
+                    isbn = value;
+                    cont = false;
+                }
+                else
+                {
+                    Console.WriteLine(value + " Tämä ISBN ei kelpaa, syötä uusi, 13 merkkiä pitkä ja aloitettava numeroilla 978");
+                        
+                    value = Console.ReadLine();
+                    cont = true;
+                }
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            
+        }
+
+        public static void ChangeTheme(string value)
+        {
+            theme = value;
+        }
+        public Book()
+        {
+            this.name = string.Empty;
+            this.author = string.Empty;
+            this.publisher = string.Empty;
+            this.Price = 0;
+            this.Isbn = string.Empty;
+            theme = string.Empty;
+        }
+
+        public Book(string name, string author, string publisher, double price, string isbn)
+        {
+            this.name = name;
+            this.author = author;
+            this.publisher = publisher;
+            this.Price = price;
+            this.Isbn = isbn;
+        }
+
+        public void GetBookDetails(string isbn)
+        {
+            if(this.Isbn == isbn)
+            {
+                Console.WriteLine(this.name + ", " + this.Author + ", " + this.publisher + ", " + this.Price.ToString("0.00") + ", " + this.Isbn + ", " + theme);
+            }
+            else
+            {
+                Console.WriteLine("Kirjan tietoja ei voida tulostaa");
+            }
+        }
+
+        
+
+    }
+}
---
+++ BookExercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using BookExercise2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Person person = new Person();
+        person.Name = "Pekka";
+
+        Console.WriteLine(person.Name);*/
+
+        // Näiden kirjojen ISBN on käypä ja menevät mutkitta 
+        Book book1 = new Book("Matikka", "Ville K", "Kalaja", 25,"978asdqwerty1");
+        Book book2 = new Book("Englanti", "Kalle V", "Kujala", 35, "978asduiower2");
+
+        // Tämä ISBN on väärä ja kysyy käyttäjää uudelleen kirjoittamaan ISBN numeron
+        Book book3 = new Book("Saksa", "Kerttu K", "Vuotsi", 15, "968sdfjlkqwe3");
+        
+        // Hakee kirjat ISBN numeron mukaan, ilmiottaa jos väärä ISBN
+        book1.GetBookDetails("978asdqwerty1");
+        book2.GetBookDetails("978asduiower2");
+        book2.GetBookDetails("9781111111111");
+
+        // Muuttaa kirjojen teeman alekirjoiksi ja tulostus näyttää tämän
+        Book.ChangeTheme("Alekirjat");
+        book1.GetBookDetails("978asdqwerty1");
+
+        // Tämä kutsuu book1 authoria
+        Console.WriteLine("Kirjan author on " + book1.Author);
+    }
+}
