1180e61661925e3435a4396df73f699c880436cb
vilihellsten <vilihellsten@hotmail.com>
2023-02-02 09:37:41 +0200

    2.2

---
+++ InterfaceExercise/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    internal class Product:IProduct
+    {
+        string name;
+        double price;
+        int count;
+
+        public Product(string name, double price, int count)
+        {
+            this.name = name;
+            this.price = price;
+            this.count = count;
+        }
+
+        public double CalculateTotal()
+        {
+            return this.price * this.count;
+        }
+
+        public Product GetProduct(string name)
+        {
+            if(this.name == name)
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string? ToString()
+        {
+            return this.name + ", " + this.price + "€, " + this.count + " kpl";
+        }
+    }
+}
---
+++ InterfaceExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using InterfaceExercise;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Product p1 = new Product("Puhelin", 150, 3);
+        Console.WriteLine(p1.GetProduct("Puhelin"));
+
+        Console.WriteLine(p1.ToString());
+        Console.WriteLine(p1.CalculateTotal());
+
+        List<Product> products = new List<Product>() {
+        new Product("Laturi",150, 4),
+        new Product("Kuulokkeet", 120, 7),
+        new Product("Kaiutin", 90, 8),
+        new Product("Kotelo", 15, 10)};
+
+        foreach (Product p in products)
+        {
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
