b91dabbd025ab986f3d6e7654651e65504637142
vilihellsten <vilihellsten@hotmail.com>
2023-02-21 21:41:44 +0200

    21.2

--- InterfaceExercise/Customer.cs
+++ InterfaceExercise/Customer.cs
@@ -32,15 +32,15 @@ namespace InterfaceExercise
         {
             if (this.purchases < 1000)
             {
-                Console.WriteLine("Bonus 2% = " + this.purchases * 0.02 + "€, ostosten määrä " + this.purchases + "€");
+                Console.WriteLine("Bonus 2% = " + Math.Round(this.purchases * 0.02,2) + "€, ostosten määrä " + this.purchases + "€");
             }
             else if (this.purchases < 2000)
             {
-                Console.WriteLine("Bonus 3% = " + this.purchases * 0.03 + "€, ostosten määrä " + this.purchases + "€");
+                Console.WriteLine("Bonus 3% = " + Math.Round(this.purchases * 0.03,2) + "€, ostosten määrä " + this.purchases + "€");
             }
             else
             {
-                Console.WriteLine("Bonus 5% =: " + this.purchases * 0.05 + "€, ostosten määrä " + this.purchases + "€");
+                Console.WriteLine("Bonus 5% =: " + Math.Round(this.purchases * 0.05,2) + "€, ostosten määrä " + this.purchases + "€");
             }
         }
 
